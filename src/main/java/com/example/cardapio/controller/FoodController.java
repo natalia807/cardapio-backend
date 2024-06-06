5e023d3613c581bed535a4242936e34b2a297f6b
natalia <natalia.santos@lucastechnologyservice.com>
2024-06-05 22:10:29 -0300

    first commit

---
+++ src/main/java/com/example/cardapio/controller/FoodController.java
@@ -0,0 +1,38 @@
+package com.example.cardapio.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.cardapio.food.Food;
+import com.example.cardapio.food.FoodRepository;
+import com.example.cardapio.food.FoodRequestDTO;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("food")
+public class FoodController {
+
+    @Autowired
+    private FoodRepository repository;
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDTO data){
+        Food foodData = new Food(data);
+        repository.save(foodData);
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping
+    public List<Food> getAll() {
+        // Converte a stream em uma lista
+        List<Food> foodList = repository.findAll();
+        return foodList;
+    }
+}
